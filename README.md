65153b5529f069d1cb4a19acafdbf210a3c561f7
Jessa <67758781+jes-sun@users.noreply.github.com>
2021-05-08 19:33:06 -0400

    Bootstrap-ified

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # The Shoppies
-I've decided to throw my hat into the ring and apply for Shopify's Fall 2021 frontend developer internship! Despite my lack of prior work experience, this is great practice for whatever future development job I get. It's also really fun.
+I've decided to throw my hat into the ring and apply for Shopify's Fall 2021 frontend developer internship! This is only, like, the second web app I've ever made, but I'm learning fast and having fun! I'm not sure if Shopify takes on interns based solely on potential and enthusiasm, but if they do, I'm a great choice :)
+
+## Requirements
+# Specifications
+
+# My Additions
+- Search results include a "More info" button that, when clicked, displays genre and plot information along with a link to the movie's IMDB page.
+- Current nominations are saved in `localStorage` so that users can leave and return to their list later.
+- Search queries are also saved in `localStorage` in case queries are revisited.
 
 ## Things I learned
 These are just some things I learned while making this project. I'm still fairly new to React (and JavaScript in general), and I figure that I'll probably remember these things better if I write them down.
 
+- My main goal for this project was to learn Bootstrap, since I didn't yet know how to make a responsive UI. Bootstrap is very useful, I like it.
+- Learned what `debounce` is and how to use it. Neat! That'll limit my API requests. Not that this'll ever hit the 1000 daily limit, but y'know. Useful.
 - React state is meant to be immutable. Don't push to a state array. Instead, I can use `concat` to create a new array and replace the old state array with this new array.
-- Some returned results contained duplicate items, which raised issues when assigning keys to search result list items. I tried using a set, but it didn't remove the seemingly duplicate items. I'm still not sure why. To solve this issue, I found the `filter` function. I used this to keep only the results that contained the first occurrence of their `imdbID`.
+- Some returned results contained duplicate items, which raised issues when assigning keys to search result list items. I tried using a set, but it didn't remove the seemingly duplicate items. I'm still not sure why. To solve this issue, I discovered the `filter` function. I used this to keep only the results that contained the first occurrence of their `imdbID`. This seems like a function I'll end up using often.
 - I learned that localStorage can only store strings, and not objects. Easy fix with `JSON.stringify()` and `JSON.parse()`.
-- I learned how to store my API key in .env so I can protect it. For ease of viewing my submission, I'll keep the API key in, but this is good knowledge moving forward.
-- This API appears to return music albums alongside the movie results. Strange.
+- I learned how to store my API key in `.env` so I can protect it. For ease of viewing my submission, I'll keep the API key in, but now I know I can hide it.
+- This API appears to return music videos alongside the movie results. Strange.
+.
--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,12 @@
 import React from "react";
+import { debounce } from "debounce";
+
+import "./Search.css";
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 class Search extends React.Component {
     constructor(props) {
@@ -7,15 +15,46 @@ class Search extends React.Component {
     }
 
     // onChanged funcs
-    onNominationsChanged = (event) => {
-        const newNominations = this.state.currentNominations.concat(event.target.value)
+    onNominationAdded = (movie) => {
+        const newNominations = this.state.currentNominations.concat(movie)
         this.setState({currentNominations:newNominations})
+    }
+    onNominationRemoved = (movie) => {
+        const newNominations = this.state.currentNominations.filter((item) => {
+            return item.imdbID !== movie.imdbID
+        })
+        this.setState({currentNominations:newNominations})
+    }
+    onNominationsChanged() {
+        localStorage.setItem("nominations", JSON.stringify(this.state.currentNominations))
+        if (this.state.currentNominations.length === 5) {
+            this.toggleMaxNominations(1)
+        } else {
+            this.toggleMaxNominations(0)
+
+            // if (this.state.currentNominations.length > 0) {
+            //     this.toggleInstructions(0)
+            // } else {
+            //     this.toggleInstructions(1)
+            // }
+        } 
+    }
+    onNominationsCleared = () => {
+        this.setState({currentNominations:[]})
     }
     onSearchChanged = (event) => {
         this.setState({currentSearch:event.target.value})
+        this.onSelectionCleared()
     }
     onSelectionChanged = (movie) => {
-        this.setState({currentSelection:movie})
+        if (movie.imdbID !== this.state.currentSelection.imdbID) {
+            this.setState({currentSelection:movie})
+        }
+        
+    }
+    onSelectionCleared() {
+        this.setState({currentSelection:{}})
+        this.toggleSelectionDetails()
     }
 
     // data funcs
@@ -27,16 +66,13 @@ class Search extends React.Component {
         const OMDBurl = "http://omdbapi.com/?apikey=" + apikey + "&type=movie&page=1&s=" + this.state.currentSearch
         
         if (this.searchCache()) {
-            console.log("retrieved from cache")
             this.setState({currentResults:JSON.parse(this.searchCache())})
         } else if (this.state.currentSearch === "") {
             localStorage.setItem("", JSON.stringify({}))
         } else {
-            console.log(OMDBurl)
             fetch(OMDBurl)
             .then((response) => response.json())
             .then((data) => {
-                console.log("data",data)
                 localStorage.setItem(this.state.currentSearch, JSON.stringify(data))
                 this.setState({currentResults:data})
             });
@@ -47,17 +83,15 @@ class Search extends React.Component {
     }
     selectionDetails = () => {
         const apikey = process.env.REACT_APP_API_KEY
-        const OMDBurl = "http://omdbapi.com/?apikey=" + apikey + "&i=" + this.state.currentSelection.imdbID
+        const OMDBurl = "http://omdbapi.com/?apikey=" + apikey + "&plot=full&i=" + this.state.currentSelection.imdbID
         
         if (this.selectionCache()) {
-            console.log("retrieved from cache")
             this.setState({currentSelection:JSON.parse(this.selectionCache())})
         } else {
             console.log(OMDBurl)
             fetch(OMDBurl)
             .then((response) => response.json())
             .then((data) => {
-                console.log("data",data)
                 localStorage.setItem(this.state.currentSelection.imdbID, JSON.stringify(data))
                 this.setState({currentSelection:data})
             });
@@ -66,26 +100,58 @@ class Search extends React.Component {
     
     // component funcs
     componentDidMount() {
-        this.searchOMDB()
-        document.getElementById("selectiondetails").style.display = "none"     
+        if (localStorage.getItem("nominations")) {
+            this.setState({currentNominations:JSON.parse(localStorage.getItem("nominations"))})
+        }
+        this.toggleSelectionDetails(0)
+        //this.toggleInstructions(1)   
+        this.toggleMaxNominations(0)
     }
     componentDidUpdate(prevProps, prevState) {
+        this.checkNominationButtons()
+
         if (this.state.currentSearch !== prevState.currentSearch) {
             this.searchOMDB()
-            console.log(this.state)
-        }
-        if (this.state.currentSelection.imdbID !== prevState.currentSelection.imdbID) {
+        } else if (this.state.currentSelection.imdbID !== prevState.currentSelection.imdbID) {
             this.selectionDetails()
-            this.showSelectionDetails()
+            this.toggleSelectionDetails(1)
+        } else if (this.state.currentNominations !== prevState.currentNominations) {  
+            this.onNominationsChanged()
         }
     }
 
-    // show/hide sections
-    showSelectionDetails() {
-        document.getElementById("selectiondetails").style.display = "block"
+    // show/hide enable/disable sections
+    toggleSelectionDetails(t) {
+        if (t) {
+            document.getElementById("selectiondetails").style.visibility = "visible"
+        } else {
+            document.getElementById("selectiondetails").style.visibility = "hidden"
+        }
+        
     }
-    showNominations() {
-
+    toggleInstructions(t) {
+        if (t) {
+            document.getElementById("instructions").style.display = "block"
+        } else {
+            document.getElementById("instructions").style.display = "none"
+        }
+        
+    }
+    toggleMaxNominations(t) {
+        // TODO
+    }
+    checkNominationButtons() {
+        const nominationButtons = document.getElementsByClassName("addnominationbutton")
+        if (this.state.currentNominations.length === 5) {
+            for (let button of nominationButtons) {
+                button.disabled = true
+            }
+        } else {
+            const currentNominationsIds = this.state.currentNominations.map(movie => movie.imdbID)
+            for (let button of nominationButtons) {
+                button.disabled = currentNominationsIds.includes(button.dataset.imdbid)
+            }
+        }
     }
 
     // additional rendering
@@ -98,90 +164,145 @@ class Search extends React.Component {
                     return resultsByID.indexOf(item.imdbID) === index
                 })
                 return uniqueSearchResults.map(movie => (
-                    <li key={movie.imdbID} className="searchResult"> 
-                        <p>
-                            {movie.Title} ({movie.Year})
-                        </p>
-                        <p id="searchresultbuttons">
-                            <button className="searchresultbutton" id="selectiondetailsbutton" onClick={this.onSelectionChanged.bind(this, movie)}>
-                                More info
-                            </button>
-                            <button className="searchresultbutton" id="nominatebutton">
+                    <Row key={movie.imdbID} className="searchresult my-2 align-items-center"> 
+                        <Col>
+                            <h6>
+                                {movie.Title} ({movie.Year})
+                            </h6>
+                        </Col>
+                        <Col id="searchresultbuttons">
+                            <button data-imdbid={movie.imdbID} className="addnominationbutton" onClick={this.onNominationAdded.bind(this, movie)}>
                                 Nominate
                             </button>
-                        </p>
-
-                    </li>
+                            <button className="selectiondetailsbutton" onClick={this.onSelectionChanged.bind(this, movie)}>
+                                More info
+                            </button>
+                        </Col>
+                    </Row>
                 ))
             } else {
                 return(
-                    <li className="noresults">
+                    <span className="noresults">
                         No results found!
-                    </li>
+                    </span>
                 )
             }
         } else {
             return(
-                <li className="noresults">
+                <span className="noresults">
                     Type something in the search bar!
-                </li>
+                </span>
             )
         }
     }
+    
     renderSelection = () => {
         if (this.state.currentSelection) {
             const currentSelection = this.state.currentSelection
             return(
-                <>
-                <h2 id="selectionttitle">
-                    {currentSelection.Title} ({currentSelection.Year})
-                </h2>
-                <em id="selectiongenre">{currentSelection.Genre}</em>
-                <p id="selectionplot">
-                    {currentSelection.Plot}
-                </p>
-                </>
+                <Row>
+                    <Col>
+                        <h4 id="selectionttitle">
+                            {currentSelection.Title} ({currentSelection.Year}) <br/> 
+                        </h4>                       
+                        <em id="selectiongenre">{currentSelection.Genre}</em> <br/>                        
+                        <p id="selectionplot">
+                            {currentSelection.Plot}
+                        </p>
+                        <a id="imdblink" href={"https://www.imdb.com/title/" + currentSelection.imdbID} rel="external">
+                            <h6>
+                                IMDB ➔
+                            </h6>
+                        </a>
+                    </Col>
+                </Row>
+            )
+        }
+    }
+    renderNominations = () => {
+        let nominationsCols = []
+        if (this.state.currentNominations.length > 0) {
+            nominationsCols = this.state.currentNominations.map(movie => (
+                <Col className="d-flex justify-content-center my-3" key={movie.imdbID}>
+                    <button className="removenominationbutton" onClick={this.onNominationRemoved.bind(this, movie)}>
+                        {this.getPoster(movie)}
+                        <h6 id="nominationtitle">
+                            {movie.Title}
+                        </h6>
+                    </button>
+                </Col>   
+                ))
+        }
+
+        while (nominationsCols.length < 5) {
+            nominationsCols.push((<Col key={"empty"+nominationsCols.length}></Col>))
+        }
+
+        return nominationsCols
+    }
+    getPoster(movie) {
+        if (movie.Poster !== "N/A") {
+            return (
+                <img src={movie.Poster} alt={movie.Title} className="nominationposter"></img>
+            )
+        } else {
+            return (
+                <div className="posterplaceholder d-flex align-items-center" style={{margin:"auto"}}>
+                    <span style={{margin:"auto"}}>
+                        No poster image
+                    </span>
+                </div>
             )
         }
     }
 
     render() {
         return (
-            <main>
-                <div id="title">
-                    <h1>The Shoppies</h1>
-                </div>
-                <div id="topsection">
-                    <div id="nominations">
-                        <h4 id="nominationstitle">
-                            Nominations
-                        </h4>
-                    </div>
-                    <div id="help">
-                        <p id="instructions">
-                            <h4 id="intro">
-                                Who will win the award for Best Movie?
-                            </h4>
-                            Nominate five of your favourite movies using the search bar below.
-                        </p>
-                    </div>
-
-                </div>
-                <div id="search">
-                    <input id="searchbar" type="search" placeholder="Search for a movie..." onChange={this.onSearchChanged}/>
-                </div>
-                <div className="results">
-                    <span id="resultslisttitle">
-                        Search Results
-                    </span>
-                    <ul id="resultslist">
+            <Container>
+                <Row>
+                    <Col>
+                        <Row id="title" className=" mt-3 justify-content-center">
+                            <h1>The Shoppies</h1>
+                        </Row>
+                        
+                        <Row id="topsection">
+                            <Col id="nominations" className="py-2">
+                                <Row>
+                                    <Col id="instructions" className="pb-2 mb-2 text-center">
+                                        <span>
+                                        Who will win the Shoppie for Best Movie? Nominate five of your favourite movies using the search bar below.
+                                        </span>
+                                    </Col>
+                                    <Col xs={2} lg={1} >
+                                        <button id="clearnominations" onClick={this.onNominationsCleared}>
+                                            Clear
+                                        </button>
+                                    </Col>
+                                </Row>
+                                <Row className="nominationslist">
+                                    {this.renderNominations()}
+                                </Row>
+                            </Col>
+                        </Row>
+                        
+                        <Row id="midsection">
+                            <Col className="py-2">
+                                <input id="searchbar" type="search" placeholder="Search for a movie..." onChange={debounce(this.onSearchChanged, 500)}/>
+                            </Col>
+                            
+                        </Row>
+                    </Col>
+                </Row>
+                <Row className="my-2 py-2" id="bottomsection">
+                    <Col id="results">
                         {this.renderResults()}
-                    </ul>
-                </div>
-                <div id="selectiondetails">
-                    {this.renderSelection()}
-                </div>
-            </main>
+                    </Col>
+                    <Col id="selectiondetails">
+                        {this.renderSelection()}
+                    </Col>
+                </Row>
+                
+            </Container>
             )
     }
     
